6b4a9b86900eb4ef7ebe3407a7436b976806e07a
Carlos Alfredo Montoya Goez <CarlosMontoya@comfama.com.co>
2022-03-01 17:33:32 -0500

    nuevas funciones

--- Juego/funciones.js
+++ Juego/funciones.js
@@ -2,40 +2,131 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const imagenes = [
         { img: './imagenes/caballo.png', name: 'caballo' },
-        { img: './imagenes/bien.png', name: 'bien' },
-        { img: './imagenes/cerdo.png', name: 'cerdo' },
-        { img: './imagenes/elefante.png', name: 'elefante' },
-        { img: './imagenes/interbb.png', name: 'interrogante' },
         { img: './imagenes/leon.png', name: 'leon' },
         { img: './imagenes/mono.png', name: 'mono' },
+        { img: './imagenes/caballo.png', name: 'caballo' },     
         { img: './imagenes/oso.png.crdownload', name: 'oso' },
-        { img: './imagenes/tigre.png', name: 'tigre' },
-        { img: './imagenes/cebra.png', name: 'cebra' },
+        { img: './imagenes/perro.png', name: 'perro' },
+        { img: './imagenes/leon.png', name: 'leon' },
+        { img: './imagenes/oso.png.crdownload', name: 'oso' },
+        { img: './imagenes/mono.png', name: 'mono' },
         { img: './imagenes/loro.png', name: 'loro' },
         { img: './imagenes/perro.png', name: 'perro' },
+        { img: './imagenes/loro.png', name: 'loro' },
     ]
 
     //tablero html
     let tablero = document.querySelector('.tablero');
 
-    //funcion para colocar las imagenes en el html a
+    let imgElegida = [];
+    let imgElegidaId = [];
+    let intentosCorrectos= [];
+    const numeroIntentoGanado = document.querySelector('.resultado')
+    
+    
 
+    //funcion para colocar las imagenes en el html a
     const crearTablero = () => {
+        
 
         for(let k=0; k < imagenes.length; k++ ){
-            
       const img = document.createElement('img')
             img.setAttribute('data-id', k);
             img.setAttribute('src', './imagenes/interbb.png');
             img.setAttribute('width', '150px');
             img.setAttribute('class','a');
-            tablero.appendChild(img)           
+            tablero.appendChild(img);
+            img.addEventListener('click',descubrirImagen);
+
         }
 
     }
+    //Método para desordenar
+    imagenes.sort(()=> 0.5 - Math.random());
+
+
+
+    //Crear funcion descubrir
+    function descubrirImagen (){
+        const imgId = this.getAttribute('data-id');
+        imgElegida.push(imagenes[imgId].name);
+        imgElegidaId.push(imgId);
+        this.setAttribute('src', imagenes[imgId].img);
+        
+        if(imgElegida.length ===2 ){
+            setTimeout(() => {
+                igualdadImg();
+            }, 500);
+        }
+
+     }
+
+     //Funcion para la igualdad
+     function igualdadImg () {
+         let imagen = document.querySelectorAll('img');
+         const opcion1Id = imgElegidaId[0];
+         const opcion2Id = imgElegidaId[1];
+
+        if(opcion1Id === opcion2Id){
+            console.log('igual');
+        }
+        else{
+            console.log('diferente');
+        }
+
+         
+
+         if(imgElegida[0] === imgElegida[1] && opcion1Id != opcion2Id ){
+             alert('Correcto');
+             imagen[opcion1Id].setAttribute('src', './imagenes/bien.png');
+             imagen[opcion2Id].setAttribute('src', './imagenes/bien.png');
+             intentosCorrectos.push(imgElegida);
+
+         }else{
+             menError1 ="Intentalode nuevo";
+             menError2 ="Debes elejir diferentes cartas";
+
+
+            alert('Intentalo de nuevo');
+            imagen[opcion1Id].setAttribute('src', './imagenes/interbb.png');
+            imagen[opcion2Id].setAttribute('src', './imagenes/interbb.png');
+            
+         }
+         imgElegida = [];
+         imgElegidaId = [];
+
+         numeroIntentoGanado.textContent=intentosCorrectos.length;
+
+         if(intentosCorrectos.length === imagenes.length/2){
+                numeroIntentoGanado.textContent = 'Ganaste!!!'
+                
+
+         }
+
+     }
+
+     const reinicio = document.querySelector('button');
+     reinicio.textContent= "Reinicar el juego"
+ 
+     reinicio.addEventListener('click', reiniciar);
+ 
+     function reiniciar () {
+          
+         console.log('Riniciar');
+         numeroIntentoGanado.textContent = 0;
+         
+       
+     }
+ 
+    //Ejecutar 
     crearTablero();
 
 
+    //crear div
+     
+
+
+   
 })
 
  
